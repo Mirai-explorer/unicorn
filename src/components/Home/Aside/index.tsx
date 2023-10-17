17edc43cd4148ddb01c79f0d7c21a9d56a6c9de9
Mirai-explorer <tangjw0226@gmail.com>
2023-10-17 15:24:36 +0800

    Project refactoring

--- src/components/Home/Aside/index.tsx
+++ src/components/Home/Aside/index.tsx
@@ -6,22 +6,26 @@ import { useDispatch } from 'react-redux';
 import { close } from '@/components/Home/Header/toggleSlice';
 import Link from 'next/link';
 
-interface navLinksType {
-    navLinks: {
-        href: string,
-        name: string
-    }[]
-}
-
-const HomeAside = ({ navLinks }: navLinksType) => {
+const HomeAside = () => {
     const toggle = useSelector((state: RootState) => state.toggle.action)
     const dispatch = useDispatch()
+    const navLinks = [
+        {
+            href: '/',
+            name: '首页'
+        },
+        {
+            href: '/music',
+            name: '云播放器'
+        }
+    ]
     return(
         <aside>
             <div className={`aside fixed top-0 ${toggle?'left-0':'left-[-300px]'} transition-all bg-white shadow h-full w-[300px] flex justify-center items-center z-[1000]`}>
                 <div className={`w-full h-full p-4 flex flex-col gap-4`}>
-                    <Link href="/">首页</Link>
-                    <Link href="/music">云播放器</Link>
+                    {navLinks.map((item, id) => (
+                        <Link href={item.href} key={id}>{item.name}</Link>
+                    ) )}
                 </div>
             </div>
             <ModalOverlay show={toggle} onClick={() => dispatch(close())}></ModalOverlay>
