36c9019515b6c9786d236d3868639be9a807e441
Mirai-explorer <tangjw0226@gmail.com>
2024-12-11 02:09:00 +0800

    [Fixes] fixed some known issues

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,29 @@
-import { MetadataRoute } from 'next'
+// app/sitemap/route.ts
+import { MetadataRoute } from 'next';
+
+export async function generateStaticParams() {
+  // 返回静态路径，例如:
+  return [
+    { params: { slug: 'music' } },
+    { params: { slug: 'blog' } },
+    { params: { slug: 'kit' } },
+    { params: { slug: 'new' } },
+  ];
+}
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://unicorn.js.org',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 1,
-        },
-        {
-            url: 'https://unicorn.js.org/music',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        }
-    ]
+  return [
+    {
+      url: 'https://unicorn.js.org',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://unicorn.js.org/music',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
 }
