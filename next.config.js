1f8f05736bf1f44fd1facdc305b357107024b9af
Mirai-explorer <tangjw0226@gmail.com>
2025-12-14 13:51:36 +0800

    chore(pages): fix pages artifact and deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require("@sentry/nextjs");
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig = {
     output: 'export',
